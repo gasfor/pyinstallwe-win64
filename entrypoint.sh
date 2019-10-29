a904d82a038760d52224122106d5c4da62c14b86
gasfor <48077766+gasfor@users.noreply.github.com>
2019-10-29 20:56:34 +0800

    add openssh-server

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,89 @@
 # Fail on errors.
 set -e
 
+#set user name\password and root password
+function __get_password ()
+{
+	local -r password_length="${1:-16}"
+
+	local password="$(
+		head -n 4096 /dev/urandom \
+		| tr -cd '[:alnum:]' \
+		| cut -c1-"${password_length}"
+	)"
+
+	printf -- '%s' "${password}"
+}
+
+function __is_valid_ssh_user ()
+{
+	local -r safe_user='^[a-z_][a-z0-9_-]{0,29}[$a-z0-9_]?$'
+	local -r user="${1}"
+
+	if [[ ${user} =~ ${safe_user} ]]
+	then
+		return 0
+	fi
+
+	return 1
+}
+
+function __get_ssh_user ()
+{
+	local -r default_value="${1:-app-admin}"
+
+	local value="${SSH_USER}"
+
+	if ! __is_valid_ssh_user "${value}"
+	then
+		value="${default_value}"
+	fi
+
+	printf -- '%s' "${value}"
+}
+
+function __get_ssh_user ()
+{
+	local -r default_value="${1:-app-admin}"
+
+	local value="${SSH_USER}"
+
+	if ! __is_valid_ssh_user "${value}"
+	then
+		value="${default_value}"
+	fi
+
+	printf -- '%s' "${value}"
+}
+
+function main ()
+{
+	local -r password_length="16"
+	ssh_user_password="${SSH_USER_PASSWORD:-"$(
+		__get_password "${password_length}"
+	)"}"
+	ssh_user="$(__get_ssh_user)"
+	ssh_root_password="$(__get_password "${password_length}")"
+	if [[ ${ssh_user} != root ]]
+	then
+		useradd -m \
+			"${ssh_user}"
+		printf -- \
+					'%s:%s\n' \
+					"${ssh_user}" \
+					"${ssh_user_password}" \
+				| chpasswd
+		echo -e "user name:${ssh_user} \n"
+		echo -e "user password:${ssh_user_password} \n"
+	fi
+	printf -- \
+			'%s:%s\n' \
+			"root" \
+			"${ssh_root_password}" \
+			| chpasswd
+	echo -e "root password:${ssh_root_password}"
+}
+
 # Make sure .bashrc is sourced
 . /root/.bashrc
 
@@ -45,3 +128,7 @@ elif [[ -f *.spec ]]; then
 else 
    echo `pyinstaller --version`
 fi
+
+main "${@}"
+
+exec /usr/sbin/sshd -D
